55799a0edc4de76298e4c45d3ba7342bb37d676c
Искандер Рыскулов <logick7@mail.ru>
2022-11-07 17:32:06 +0300

    3

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'api.apps.ApiConfig'
+    'api'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig',
+    'posts',
     'rest_framework',
     'rest_framework.authtoken',
 ]
